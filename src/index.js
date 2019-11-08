e6e1fa204fbbffeefaa4c13d178b64f49e0d24e8
Andre Noberto <andre-noberto@hotmail.com>
2019-11-08 16:27:27 -0300

    Importing TwitterService

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-const app = require('./app');
 
+const app = require('./app');
 const { server } = require('./configuration');
+const { TwitterService } = require('./services');
 
 app.listen(server.port, () => {
   console.info(`Server started listening at port ${server.port}`);
