5bca324eff7a4caaab31137ba3535573f7f0117b
Andre Noberto <andre-noberto@hotmail.com>
2019-11-09 10:32:59 -0300

    Updating Twitter service architecture

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 require('dotenv').config();
 
 const app = require('./app');
-const { server, twitter } = require('./configuration');
-const { TwitterService } = require('./services');
+const { server } = require('./configuration');
 
-app.listen(server.port, async () => {
-  let twitterService;
-
-  console.info(`Server started listening at port ${server.port}`);
-  console.info('Getting Twitter access token...');
-
-  try {
-    //const tweets = await TwitterService.searchTweetsWithHashtags('liveperson');
-    //console.log(tweets.statuses[0])
-  } catch (err) {
-    console.error(err);
-  }
-});
+app.listen(server.port, () => console.info(`Server started listening at port ${server.port}`));
--- src/services/twitter.js
+++ src/services/twitter.js
@@ -2,39 +2,19 @@ const { twitter } = require('../configuration');
 const { encodeCredentials, request } = require('../helpers');
 
 class TwitterService {
-  constructor(accessToken) {
-    this.accessToken = accessToken;
-    console.log('Access Token:', accessToken);
-  }
-
-  static async getAccessToken() {
-    const basicToken = encodeCredentials(twitter.consumerApi.key, twitter.consumerApi.secret);
-    const options = {
-      uri: `${twitter.api.baseUrl}/oauth2/token?grant_type=client_credentials`,
-      headers: {
-        'Authorization': `Basic ${basicToken}`,
-      },
-      method: 'POST',
-      qs: {
-        'grant_type': 'client_credentials',
-      },
-    };
-
-    return await request(options);
-  }
-
   async searchTweetsWithHashtags(...tags) {
+    const accessToken = await this.getAccessToken();
     const hashtags = this.addHashSymbol(tags);
     const queryString = hashtags.join('+');
     const options = {
       uri: `${twitter.api.baseUrl}/${twitter.api.version}/search/tweets.json`,
       headers: {
-        'Authorization': `Bearer ${this.accessToken}`,
+        'Authorization': `Bearer ${accessToken}`,
       },
       method: 'GET',
       qs: {
-        'q': queryString
-      }
+        'q': queryString,
+      },
     };
 
     try {
@@ -47,8 +27,45 @@ class TwitterService {
   addHashSymbol(...tags) {
     return tags.map(tag => `#${tag}`);
   }
+
+  async getAccessToken() {
+    if (this.isAccessTokenAvailable()) {
+      return this.accessToken;
+    }
+
+    await this.requestAccessToken();
+
+    return this.accessToken;
+  }
+
+  isAccessTokenAvailable() {
+    return !!this.accessToken;
+  }
+
+  async requestAccessToken() {
+    try {
+      const basicToken = encodeCredentials(twitter.consumerApi.key, twitter.consumerApi.secret);
+      const options = {
+        uri: `${twitter.api.baseUrl}/oauth2/token?grant_type=client_credentials`,
+        headers: {
+          'Authorization': `Basic ${basicToken}`,
+        },
+        method: 'POST',
+        qs: {
+          'grant_type': 'client_credentials',
+        },
+      };
+      const { access_token } = await request(options);
+      
+      this.setAccessToken(access_token);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  setAccessToken(accessToken) {
+    this.accessToken = accessToken;
+  }
 }
 
-const accessToken = async () => await TwitterService.getAccessToken();
-
-module.exports = new TwitterService(accessToken);
+module.exports = new TwitterService();
